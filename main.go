2f96409e0fda9dd578120770fe9d4af643f12a35
Thomas Baloyi <baloyi.thomas@outlook.com>
2023-08-31 21:00:16 +0200

    Chart out objectives

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// TODO: Tariff table
+
+	// TODO: Keep program alive and close only on q
+
+	// TODO: Advance a clock by x amount of time from user input using "a" or "advance"
+
+	// TODO: Arrive at a car park using "arrive", calculate fair based on duration and tariff table
+
+	// TODO: Depart car park
+
+	fmt.Println("Car Park Sim")
+}
