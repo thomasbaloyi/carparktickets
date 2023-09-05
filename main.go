f4ce6fedcf31dfd58afb5283ed1a69271bc82285
Thomas Baloyi <baloyi.thomas@outlook.com>
2023-09-05 20:54:58 +0200

    Keep program alive and close only on q

--- main.go
+++ main.go
@@ -13,22 +13,33 @@ func main() {
 		Duration int64
 	}
 
-	var tariffs = []Tariff{
-		{Cost: 10, Duration: 1000},
-		{Cost: 20, Duration: 3000},
-		{Cost: 30, Duration: 6000},
-		{Cost: 50, Duration: 8000},
-		{Cost: 90, Duration: 9000},
-		{Cost: 80, Duration: 10000},
-	}
+	// var tariffs = []Tariff{
+	// 	{Cost: 10, Duration: 1000},
+	// 	{Cost: 20, Duration: 3000},
+	// 	{Cost: 30, Duration: 6000},
+	// 	{Cost: 50, Duration: 8000},
+	// 	{Cost: 90, Duration: 9000},
+	// 	{Cost: 80, Duration: 10000},
+	// }
 
-	// TODO: Keep program alive and close only on q
+	// Totally new to me, Go does not have a while loop, can be simulated as follows:
+	for {
+		var userInput = requestUserInput()
+		if userInput == "q" {
+			break
+		}
+	}
 
 	// TODO: Advance a clock by x amount of time from user input using "a" or "advance"
 
 	// TODO: Arrive at a car park using "arrive", calculate fair based on duration and tariff table
 
 	// TODO: Depart car park
+}
 
-	fmt.Println("Car Park Sim")
+func requestUserInput() string {
+	var userInput string
+	fmt.Println("Enter a command: ")
+	fmt.Scan(&userInput)
+	return userInput
 }
